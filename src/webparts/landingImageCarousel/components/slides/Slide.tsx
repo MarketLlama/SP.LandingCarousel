22d6deefce2a8d2e04b1be9533b38615791214e9
Mark Williams <mark@Marks-MacBook-Pro.local>
2019-05-08 11:58:26 +0100

    Mobile Fixes

--- src/webparts/landingImageCarousel/components/slides/Slide.tsx
+++ src/webparts/landingImageCarousel/components/slides/Slide.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {ISlideProps} from '.';
+import { ISlideProps } from '.';
 import styles from './../LandingImageCarousel.module.scss';
 
 export class Slide extends React.Component<ISlideProps, {}> {
@@ -15,12 +15,14 @@ export class Slide extends React.Component<ISlideProps, {}> {
     textStyle.color = this.props.textColor;
 
     return (
-      <div className={styles.slideImage} style={slideStyle}>
-        <a href={this.props.item.url? this.props.item.url : null}
-          target={this.props.item.target? this.props.item.target : null}>
-          <div className={styles.slideText} style={textStyle} >
-            <h1>{this.props.item.title}</h1>
-            <p>{this.props.item.description}</p>
+      <div>
+        <a href={this.props.item.url ? this.props.item.url : null}
+          target={this.props.item.target ? this.props.item.target : null}>
+          <div className={styles.slideImage} style={slideStyle}>
+            <div className={styles.slideText} style={textStyle} >
+              <h1>{this.props.item.title}</h1>
+              <p>{this.props.item.description}</p>
+            </div>
           </div>
         </a>
       </div>
